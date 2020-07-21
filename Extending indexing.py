9a01962984b37df77e73448793d219ffd38b952e
Ruhi Awasthi <ruhiawasthi93rd@gmail.com>
2020-07-21 11:40:17 +0530

    Adding more files related to slicing

---
+++ Extending indexing.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+
+# Python program to demonstrate 
+# string slicing 
+  
+# String slicing  
+String ='ASTRING'
+  
+# Using indexing sequence 
+print(String[:3]) 
+print(String[1:5:2]) 
+print(String[-1:-12:-2]) 
+  
+# Prints string in reverse  
+print("\nReverse String") 
+print(String[::-1]) 
+
